0986fb3c447122bb6453317d4bd128a280f035d5
Manol Donev <manol.donev@gmail.com>
2020-07-08 12:43:11 +0300

    feat: add repeated string challenge

---
+++ challenges_2020/repeated_string.py
@@ -0,0 +1,24 @@
+
+"""Repeated string"""
+
+
+def repeated_string(s, n):
+    """https://www.hackerrank.com/challenges/repeated-string"""
+
+    string_length = len(s)
+    m = n // string_length
+    remainder = n % string_length
+    result = 0
+    for index, char in enumerate(s):
+        if char != 'a':
+            continue
+
+        if index < remainder:
+            result += m + 1
+        else:
+            result += m
+
+    return result
+
+
+print(repeated_string("aab", 882787))
