5b5916a6752d117dc25ca37f6f7bdaa28a87b003
Manol Donev <manol.donev@gmail.com>
2020-07-08 12:42:33 +0300

    feat: add counting valleys challenge

---
+++ challenges_2020/counting_valleys.py
@@ -0,0 +1,23 @@
+
+"""Counting Valleys"""
+
+
+def counting_valleys(n, s):
+    """https://www.hackerrank.com/challenges/counting-valleys"""
+
+    valley_count = 0
+    level = 0
+
+    for step in s:
+        if step == 'D':
+            level -= 1
+        else:
+            level += 1
+
+        if level == 0 and step == 'U':
+            valley_count += 1
+
+    return valley_count
+
+
+print(counting_valleys(8, "UDDDUDUU"))
