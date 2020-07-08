689026459bd3a4d1e21029ab046e2533d2fa21f0
Manol Donev <manol.donev@gmail.com>
2020-07-08 15:43:50 +0300

    feat: add max array sum challenge solution

---
+++ challenges_2020/max_array_sum.py
@@ -0,0 +1,20 @@
+
+"""Max Array Sum"""
+
+import itertools
+
+
+def max_subset_sum(arr):
+    """https://www.hackerrank.com/challenges/max-array-sum"""
+
+    max_sums = [0, max(0, arr[0])]
+
+    index = 2
+    for value in itertools.islice(arr, 1, None):
+        max_sums.append(max(max_sums[index - 1], value + max_sums[index - 2]))
+        index += 1
+
+    return max_sums[-1]
+
+
+print(max_subset_sum([-2, 1, 3, -4, 5]))
