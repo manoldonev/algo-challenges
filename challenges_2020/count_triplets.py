5eccc62591cd79cb2183a4968523e53e1c9d78b6
Manol Donev <manol.donev@gmail.com>
2020-07-08 12:42:18 +0300

    feat: add count triplets challenge

---
+++ challenges_2020/count_triplets.py
@@ -0,0 +1,28 @@
+
+"""Count triplets"""
+
+from collections import Counter
+
+
+def count_triplets(arr, r):
+    """https://www.hackerrank.com/challenges/count-triplets-1"""
+
+    counter_left = Counter()
+    counter_right = Counter(arr)
+    result = 0
+
+    for j in arr:
+        i = j // r
+        k = j * r
+
+        counter_right[j] -= 1
+
+        if counter_left[i] and counter_right[k] and not j % r:
+            result += counter_left[i] * counter_right[k]
+
+        counter_left[j] += 1
+
+    return result
+
+
+print(count_triplets([1, 5, 5, 25, 125], 5))
