b80e73001bfdd25bdb42ba7603eadd3f3377436b
Manol Donev <manol.donev@gmail.com>
2020-10-12 21:50:19 +0300

    chore: add comment

--- src/pascals_triangle_ii.py
+++ src/pascals_triangle_ii.py
@@ -6,6 +6,8 @@ from operator import add
 
 
 def get_row(row_index: int) -> List[int]:
+    """https://leetcode.com/problems/pascals-triangle-ii/"""
+
     current_row = [1]
     for _ in range(1, row_index + 1):
         map_ = map(add, [0] + current_row, current_row + [0])
