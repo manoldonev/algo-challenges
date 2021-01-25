cc7d38e490e2b33acfa530271d2009a7f5b9f1ea
Manol Donev <manol.donev@gmail.com>
2021-01-25 12:36:50 +0200

    refactor: generalize 4sum solution

--- src/four_sum.py
+++ src/four_sum.py
@@ -7,52 +7,37 @@ from typing import List
 def find_four_sum(numbers: List[int], target: int) -> List[List[int]]:
     """https://leetcode.com/problems/4sum/"""
 
-    result = []
     numbers.sort()
-
-    n = len(numbers)
-    target_sentinel = target // 4
-    target_sentinel2 = target // 2
-    for pivot_index in range(n):
-        pivot1 = numbers[pivot_index]
-        if pivot1 > target_sentinel:
-            break
-
-        if pivot_index > 0 and \
-                numbers[pivot_index - 1] == numbers[pivot_index]:
-            continue
-
-        for secondary_pivot_index in range(pivot_index + 1, n):
-            pivot2 = numbers[secondary_pivot_index]
-            if pivot1 + pivot2 > target_sentinel2:
-                break
-
-            if secondary_pivot_index == pivot_index + 1 or \
-                    numbers[secondary_pivot_index - 1] != numbers[secondary_pivot_index]:
-                remaining_target = target - pivot1 - pivot2
-                for pair in _find_two_sum(numbers, secondary_pivot_index, remaining_target):
-                    result.append([pivot1, pivot2, *pair])
+    n_sum = 4
+    result = []
+    find_n_sum(numbers, target, 0, len(numbers) - 1, n_sum, [], result)
 
     return result
 
 
-def _find_two_sum(numbers: List[int], pivot_index: int, target: int) -> List[List[int]]:
-    n = len(numbers)
-    left = pivot_index + 1
-    right = n - 1
+def find_n_sum(numbers: List[int], target: int, left: int, right: int, n_sum: int, current: List[int], result: List[List[int]]):
+    if right - left + 1 < n_sum or \
+            target < numbers[left] * n_sum or target > numbers[right] * n_sum:
+        return
 
-    while left < right:
-        current = numbers[left] + numbers[right]
-        if current == target:
-            yield [numbers[left], numbers[right]]
-            left += 1
-            right -= 1
-            while left < right and numbers[left] == numbers[left - 1]:
+    if n_sum == 2:
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum == target:
+                result.append(current + [numbers[left], numbers[right]])
                 left += 1
-
-            while left < right and numbers[right] == numbers[right + 1]:
                 right -= 1
-        elif current > target:
-            right -= 1
-        else:
-            left += 1
+                while left < right and numbers[left] == numbers[left - 1]:
+                    left += 1
+
+                while left < right and numbers[right] == numbers[right + 1]:
+                    right -= 1
+            elif current_sum > target:
+                right -= 1
+            else:
+                left += 1
+    else:
+        for i in range(left, right + 1):
+            if i == left or numbers[i - 1] != numbers[i]:
+                find_n_sum(
+                    numbers, target - numbers[i], i + 1, right, n_sum - 1, current + [numbers[i]], result)
