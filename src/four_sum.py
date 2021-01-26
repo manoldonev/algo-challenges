d19c8230ceafc675df06ae5d38549239a528b5f6
Manol Donev <manol.donev@gmail.com>
2021-01-26 14:52:20 +0200

    refactor: naming

--- src/four_sum.py
+++ src/four_sum.py
@@ -8,19 +8,19 @@ def find_four_sum(numbers: List[int], target: int) -> List[List[int]]:
     """https://leetcode.com/problems/4sum/"""
 
     numbers.sort()
-    n_sum = 4
+    k_sum = 4
     result = []
-    find_n_sum(numbers, target, 0, len(numbers) - 1, n_sum, [], result)
+    find_k_sum(numbers, target, 0, len(numbers) - 1, k_sum, [], result)
 
     return result
 
 
-def find_n_sum(numbers: List[int], target: int, left: int, right: int, n_sum: int, current: List[int], result: List[List[int]]):
-    if right - left + 1 < n_sum or \
-            target < numbers[left] * n_sum or target > numbers[right] * n_sum:
+def find_k_sum(numbers: List[int], target: int, left: int, right: int, k_sum: int, current: List[int], result: List[List[int]]):
+    if right - left + 1 < k_sum or \
+            target < numbers[left] * k_sum or target > numbers[right] * k_sum:
         return
 
-    if n_sum == 2:
+    if k_sum == 2:
         while left < right:
             current_sum = numbers[left] + numbers[right]
             if current_sum == target:
@@ -39,5 +39,5 @@ def find_n_sum(numbers: List[int], target: int, left: int, right: int, n_sum: in
     else:
         for i in range(left, right + 1):
             if i == left or numbers[i - 1] != numbers[i]:
-                find_n_sum(
-                    numbers, target - numbers[i], i + 1, right, n_sum - 1, current + [numbers[i]], result)
+                find_k_sum(
+                    numbers, target - numbers[i], i + 1, right, k_sum - 1, current + [numbers[i]], result)
