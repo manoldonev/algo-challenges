2465661efd4b5311724ce457565208baa4c51e25
Manol Donev <manol.donev@gmail.com>
2021-01-26 15:22:39 +0200

    refactor: add generalized solution

--- src/four_sum_ii.py
+++ src/four_sum_ii.py
@@ -8,21 +8,37 @@ from typing import List
 def count_four_sum(A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
     """https://leetcode.com/problems/4sum-ii/"""
 
-    result = 0
-    counter = Counter()
-    a_counter = Counter(A)
-    b_counter = Counter(B)
-    c_counter = Counter(C)
-    d_counter = Counter(D)
+    counter_ab = Counter(a+b for a in A for b in B)
+    return sum(counter_ab[-(c+d)] for c in C for d in D)
 
-    for a in a_counter:
-        for b in b_counter:
-            counter[a+b] += a_counter[a] * b_counter[b]
 
-    for c in c_counter:
-        for d in d_counter:
-            complement = -(c+d)
-            if complement in counter:
-                result += counter[complement] * c_counter[c] * d_counter[d]
+# def count_four_sum(A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+#     """https://leetcode.com/problems/4sum-ii/"""
 
-    return result
+#     return _count_k_sum([A, B, C, D])
+
+
+# def _count_k_sum(lists: List[List[int]]) -> int:
+#     counter = Counter()
+#     _add_to_counter(lists, 0, 0, counter)
+#     return _count_complements(lists, len(lists) // 2, 0, counter)
+
+
+# def _add_to_counter(lists: List[List[int]], index: int, half_sum: int, counter: Counter):
+#     if index == len(lists) // 2:
+#         counter[half_sum] += 1
+#     else:
+#         for value in lists[index]:
+#             _add_to_counter(lists, index + 1, half_sum + value, counter)
+
+
+# def _count_complements(lists: List[List[int]], index: int, complement: int, counter: Counter) -> int:
+#     if index == len(lists):
+#         return counter[complement]
+
+#     result = 0
+#     for value in lists[index]:
+#         result += _count_complements(lists, index + 1,
+#                                      complement - value, counter)
+
+#     return result
--- src/test_four_sum_ii.py
+++ src/test_four_sum_ii.py
@@ -15,6 +15,10 @@ class FourSumIiTests(unittest.TestCase):
         self.assertEqual(count_four_sum(
             [0, 1, -1], [-1, 1, 0], [0, 0, 1], [-1, 1, 1]), 17)
 
+    def test_case_3(self):
+        self.assertEqual(count_four_sum(
+            [-1, -1], [-1, 1], [-1, 1], [1, -1]), 6)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
