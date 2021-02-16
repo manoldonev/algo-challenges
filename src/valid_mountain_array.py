cc67ca530ff1a832a1292f90444f3dff29fdba2a
Manol Donev <manol.donev@gmail.com>
2021-02-16 15:06:05 +0200

    refactor: typing

--- src/valid_mountain_array.py
+++ src/valid_mountain_array.py
@@ -1,24 +1,26 @@
 
 """Valid Mountain Array"""
 
+from typing import List
 
-def is_valid_mountain_array(arr):
+
+def is_valid_mountain_array(numbers: List[int]) -> bool:
     """https://leetcode.com/problems/valid-mountain-array"""
 
-    n = len(arr)
+    n = len(numbers)
     if n < 3:
         return False
 
     left = 0
     right = n - 1
 
-    while left < n - 1 and arr[left] < arr[left + 1]:
+    while left < n - 1 and numbers[left] < numbers[left + 1]:
         left += 1
 
     if left in (0, right):
         return False
 
-    while left < right and arr[left] > arr[left + 1]:
+    while left < right and numbers[left] > numbers[left + 1]:
         left += 1
 
     return left == right
